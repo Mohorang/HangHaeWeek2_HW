fe77a2899f5a7f1daf56877e4bdabea0e4175f79
Mohorang <dlwnsgh253@gmail.com>
2022-05-19 20:19:34 +0900

    항해99 2주차 HW 수정 및 테스트 내용 추가 수정내용 : 상속을 이용하여 버스와 택시 클래스 구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,53 +3,121 @@ import java.util.List;
 
 //승객이 입장하는 메소드를 인터페이스로 만들어서 각자 다른동작을 하게끔 해보았다.
 
-interface passengerIn{
-    void passengerIn();
-}
-class Bus implements passengerIn{
-    int maxPassenger;
-    int curPassenger;
-    int price;
-    int busNum;
+
+abstract class vehicle{
+    int num;
     int fuelVolume;
     int speed;
+    int maxPassenger;
     boolean status;
+    abstract public void run();
+    abstract public void changeStatus();
+    abstract public void passengerIn(int passenger);
+    abstract public void changeFuel(int Fuel);
 
+
+
+}
+class Bus extends vehicle{
+
+    int curPassenger;
+    int price;
+    int maxPrice;
+    List<Integer> busList = new ArrayList<>();
+    //버스번호 중복하는지 감시하는 메소드
+    private boolean checkBusNum(int busnum){
+        //버스를 체크해서 중복이 있으면 fasle리턴
+        for(int i=0; i<busList.size(); i++){
+            if(busnum == busList.get(i))
+            {
+                System.out.println("버스번호가 중복입니다.");
+                System.out.println("---------------------");
+                return false;
+            }
+        }
+        busList.add(busnum);
+        return true;
+    }
     //Bus클래스의 생성자
     Bus(int busNum , int fuelVolume,int speed,boolean status,int curPassenger ,int maxPassenger, int price){
-        this.busNum = busNum;
+        if(!checkBusNum(busNum)) {
+            return;
+        }
+        else
+        {
+            System.out.println("버스 번호" + busNum + "이 생성 되었습니다.");
+            System.out.println("---------------------");
+        }
         this.fuelVolume = fuelVolume;
         this.speed = speed;
         //status가 true인것은 운행을 의미
         this.status = true;
-        this.curPassenger = curPassenger;
+        this.curPassenger = 0;
         this.maxPassenger = maxPassenger;
         this.price = price;
+        //버스 생성자 만들때 탑승한 고객수 * price가 초기돈
+        this.maxPrice = 0;
+        passengerIn(curPassenger);
+        changeStatus();
     }
-    @Override
-    public void passengerIn() {
+    public void changeFuel(int Fuel){
+        fuelVolume += Fuel;
+        changeStatus();
+        if(!status)
+            System.out.println("상태 = 차고지행");
+        else System.out.println("상태 = 운행중");
+        System.out.println("현재 주유량 = " + fuelVolume);
+        System.out.println("---------------------");
+
+    }
+    public void passengerIn(int passenger) {
         //현재 승객수가 최대승객수와 같거나
-        if(this.curPassenger == this.maxPassenger) return;
-            //status가 차고지행인 상태에서는 승객수를 증가시키지 않는다.
-        else if(this.status == false) return;
+        if((this.curPassenger + passenger) > this.maxPassenger)
+        {
+            System.out.println("최대 승객수를 초과하였습니다!");
+            System.out.println("---------------------");
+            return;
+        }
+        //status가 차고지행인 상태에서는 승객수를 증가시키지 않는다.
+        else if(this.status == false)
+        {
+            System.out.println("차를 운행할 수 없는 상태입니다!");
+            System.out.println("---------------------");
+            return;
+        }
             //그 이외의 경우에만 승객수가 증가한다.
-        else this.curPassenger++;
+        else
+        {
+            //price*탑승한 고객수 만큼 번 요금 증가
+            maxPrice += price * passenger;
+            //현재 승객수 passenger만큼 증가
+            this.curPassenger += passenger;
+            System.out.println("탑승 승객수 =" + this.curPassenger);
+            System.out.println("잔여 승객수 =" + (maxPassenger - this.curPassenger));
+            System.out.println("요금확인 =" + maxPrice);
+            System.out.println("---------------------");
+        }
     }
 
     //버스 상태 변경
     public void changeStatus(){
+        //운행중이였으면 조건없이 차고지행으로
+        //주유량 검사
         if(this.fuelVolume < 10)
         {
             //주유량이 기준치에 못미치면 메세지를 print하고 운행상태를 차고지행으로 변경
             status = false;
             //주유가 필요합니다. 메세지 출력
             System.out.println("주유가 필요합니다.");
+            System.out.println("운행을 정지합니다.");
+            System.out.println("---------------------");
+            return;
         }
-        //주유 조건을 만족햇을시 버스상태변경 운행중이면 차고지행 , 차고지행이면 운행으로 변경
-        this.status = true ? false : true;
+        //조건을 만족하면 운행중으로 변경
+        else this.status = true;
+
     }
     //승객 탑승
-
     //속도 변경
     public void changeSpeed(int speed)
     {
@@ -57,31 +125,30 @@ class Bus implements passengerIn{
         {
             //주유량이 기준치에 못미치면 메세지를 print하고 운행상태를 차고지행으로 변경
             //기준치는 임의로 10이라 설정
-            status = false;
             System.out.println("주유량을 확인해 주세요.");
+            System.out.println("---------------------");
+            driveOver();
         }
         //변경하고싶은 speed값을 입력받아 속도변경
         this.speed = speed;
     }
 
-    public void runBus(){
+    public void run(){
         //status의 true는 운행을 의미 false는 차고지행
         if(!(this.fuelVolume < 10)) status = true;
     }
     public void driveOver(){
         //운행 종료시 차고지행
+        this.curPassenger = 0;
+        this.maxPrice = 0;
+        System.out.println("운행을 정지합니다.");
+        System.out.println("---------------------");
         this.status = false;
     }
 }
 
 
-class Taxi implements passengerIn{
-    //택시번호
-    int taxiNum;
-    //주유량
-    int fuelValue;
-    //현재속도
-    int speed;
+class Taxi extends vehicle{
     //목적지
     String destination;
     //기본 거리
@@ -94,10 +161,29 @@ class Taxi implements passengerIn{
     int pricePerDistance;
     //status true = 운행 중 , false 일반
     boolean status;
-
+    List<Integer> taxiList = new ArrayList<>();
+    private boolean checkBusNum(int busnum){
+        //버스를 체크해서 중복이 있으면 fasle리턴
+        for(int i=0; i<taxiList.size(); i++){
+            if(busnum == taxiList.get(i))
+            {
+                System.out.println("택시번호가 중복입니다.");
+                System.out.println("---------------------");
+                return false;
+            }
+        }
+        taxiList.add(busnum);
+        return true;
+    }
     public Taxi(int taxiNum, int fuelValue, int speed, String destination, double normalDistance, double distanceToDestination, int normalPrice, int pricePerDistance, boolean status) {
-        this.taxiNum = taxiNum;
-        this.fuelValue = fuelValue;
+        if(!checkBusNum(taxiNum)) {
+            return;
+        }
+        else {
+            System.out.println("택시 번호" + taxiNum + "이 생성 되었습니다.");
+            System.out.println("---------------------");
+        }
+        this.fuelVolume = fuelValue;
         this.speed = speed;
         this.destination = destination;
         this.normalDistance = normalDistance;
@@ -107,18 +193,54 @@ class Taxi implements passengerIn{
         //초기상태는 일반
         this.status = false;
     }
-
+    public void changeFuel(int Fuel){
+        fuelVolume += Fuel;
+        if(!status)
+            System.out.println("상태 = 연료부족으로 인한 일반상태 전환");
+        else System.out.println("상태 = 연료 문제없음");
+        System.out.println("현재 주유량 = " + fuelVolume);
+        System.out.println("---------------------");
+    }
+    public void changeStatus(){
+        //운행중이였으면 조건없이 일반으로
+        //주유량 검사
+        if(this.fuelVolume < 10)
+        {
+            //주유량이 기준치에 못미치면 메세지를 print하고 운행상태를 차고지행으로 변경
+            status = false;
+            //주유가 필요합니다. 메세지 출력
+            System.out.println("주유가 필요합니다.");
+            System.out.println("운행을 정지합니다.");
+            System.out.println("---------------------");
+            return;
+        }
+    }
+    public void driveOver(){
+        System.out.println("운행을 정지합니다.");
+        System.out.println("---------------------");
+        this.status = false;
+    }
     //운행시작 기능조건 : 주유량 10이상
-    public void runTaxi(){
-        if(!(this.fuelValue < 10)) status = true;
+    public void run()
+    {
+        if(!(this.fuelVolume < 10)) status = true;
     }
     //승객탑승
-    @Override
-    public void passengerIn(){
+    public void passengerIn(int passenger){
         //택시상태가 일반일때만 입장가능 및 상태를 운행중으로 변경
-        if(!status) status = true;
-        else System.out.println("탑승이 불가능합니다.");
+        if(!status)
+        {
+            //운행중으로 상태변경
+            status = true;
+        }
+        //운행중일땐 탑승불가
+        else
+        {
+            System.out.println("탑승이 불가능합니다.");
+            System.out.println("---------------------");
+        }
     }
+
     //속도 변경
     public void changeSpeed(int speed){
         //현재속도보다 느린경우
@@ -138,6 +260,7 @@ class Taxi implements passengerIn{
         //(기본요금 * 기본거리) + (추가요금 * (목적지거리-기본거리))
         total = (int)(normalPrice * normalDistance) + (int)(pricePerDistance * addDistance);
         System.out.println("요금은" + total + "원입니다.");
+        System.out.println("---------------------");
         return total;
     }
 
@@ -150,5 +273,15 @@ public class Main {
         numOfBus.add(1);
         //택시도 동일
         numOfTaxi.add(1);
+
+        Bus bus1 = new Bus(1,100,0,true,5,30,1000);
+        bus1.passengerIn(2);
+        bus1.changeFuel(-50);
+        bus1.driveOver();
+        bus1.changeFuel(10);
+        bus1.run();
+        bus1.passengerIn(45);
+        bus1.passengerIn(5);
+        bus1.changeFuel(-55);
     }
 }
